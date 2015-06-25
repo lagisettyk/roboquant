6167c341ce33baff6bae9276ed9a73911416bf4f
Kiran <lagisetty.kiran@gmail.com>
2015-06-25 13:30:15 -0700

    Modified ticker list to be only 4 stocks

--- utils/util.py
+++ utils/util.py
@@ -31,10 +31,10 @@ def getTickerListWithSPY():
 
 def getTickerList():
 
-	#tickerList = ['AAPL', 'NFLX', 'GOOGL', 'FDX']
+	tickerList = ['AAPL', 'NFLX', 'GOOGL', 'FDX']
 
-	tickerList = ['AAPL', 'AMZN', 'FDX', 'MA', 'NFLX', 'OCR', 'GD','NXPI', 'CVS', 'UNP', 'GILD', 'VRX', 'ACT', 'XLF','GOOGL', 'CF', 'URI', 'CP', 'WHR', 'IWM', 'UNH', 'VIAB', 'FLT', \
-	 'ODFL', 'ALL', 'V']
+	#tickerList = ['AAPL', 'AMZN', 'FDX', 'MA', 'NFLX', 'OCR', 'GD','NXPI', 'CVS', 'UNP', 'GILD', 'VRX', 'ACT', 'XLF','GOOGL', 'CF', 'URI', 'CP', 'WHR', 'IWM', 'UNH', 'VIAB', 'FLT', \
+	# 'ODFL', 'ALL', 'V']
 
 	'''
 	file_tickerlist = getRelativePath('cboesymbol.csv')
