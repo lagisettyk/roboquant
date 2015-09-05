a01634bc3b3cfe7f6bdd1acb2a57a71dd201c884
Kiran <lagisetty.kiran@gmail.com>
2015-09-04 18:07:30 -0700

    Data Scheduler cron schedules decorators commented out

--- data_scheduler.py
+++ data_scheduler.py
@@ -45,7 +45,7 @@ def scheduled_job():
 	print job.result   # => 889
 
 
-@sched.scheduled_job('date')
+#@sched.scheduled_job('date')
 #@sched.scheduled_job('cron', day_of_week='mon-fri', hour=17)
 def populate_moneyflow():
 	print('This job is to populate cashflow')
